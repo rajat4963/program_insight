006472f495e711d049d77ab3985789b7d94bd5f7
rajat4963 <b140064@nitsikkim.ac.in>
2018-06-27 18:15:35 +0530

    Add files via upload

---
+++ calcnj.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./t2.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHead(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(5060);
+});
